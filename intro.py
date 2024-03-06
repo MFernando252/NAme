03344a23067224aea3bc18e572e28ea75f226a77
HussamShah10 <145381883+HussamShah10@users.noreply.github.com>
2024-03-06 13:52:53 -0500

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,58 @@
+import random
+
+def main():
+    Starting_money = 1000
+    debt = random.randint(10000, 20000)
+    Has_money = True
+
+    print("---------------")
+
+    print(name)
+    print(f"Starting money ${Starting_money}")
+    print(f"Your debt $-{debt}")
+
+    print("")
+    print("---------------")
+main()
+
+
+
+
+def game_play():
+
+game_play()
+
+
+
+
+
+def tictactoe():
+
+tictactoe()
+
+
+
+
+
+def rockpapersiccor():
+
+rockpapersiccor()
+
+
+
+
+
+
+def plinko():
+
+plinko()
+
+
+
+
+def gameOVer():
+
+gameOVer()
+
+
+
