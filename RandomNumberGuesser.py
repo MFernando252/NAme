482634c6f2b804019b97d99f08fff797744e1e1f
MFernando252 <145598879+MFernando252@users.noreply.github.com>
2024-03-07 23:45:36 -0500

    Update RandomNumberGuesser.py

--- RandomNumberGuesser.py
+++ RandomNumberGuesser.py
@@ -59,49 +59,54 @@ def main():
     def Number_Guesser():
         # SIZE OF THELIST GIVEN FROM USER INPUT.
         list_size = []
-
-        number_inlist = int(input("Hello! From 2-5 How Low Or High Do You Want Your Odds? "))
-
+        cash_gambled = int(input("How much cash do you wanna wager? "))
+        number_inlist = int(input(f"Hello! From 2-5 How Low Or High Do You Want Your Odds?\n "
+                                  f"Note, higher number means higher payout!"))
         # APPENDS TO LIST (I think code can be simplified lmao)
         if number_inlist not in range(2,6):
             print("Number not found.")
         elif number_inlist == 2:
             list_size.append(1)
             list_size.append(2)
-            reward = 30
+            reward = 1.1
             print(list_size)
         elif number_inlist == 3:
             list_size.append(1)
             list_size.append(2)
             list_size.append(3)
-            print(list_size)
+            reward = 1.2
         elif number_inlist == 4:
             list_size.append(1)
             list_size.append(2)
             list_size.append(3)
             list_size.append(4)
-            print(list_size)
+            reward = 1.4
         elif number_inlist == 5:
             list_size.append(1)
             list_size.append(2)
             list_size.append(3)
             list_size.append(4)
             list_size.append(5)
-            print(list_size)
-        # ERROR CHECKER?
+            reward = 1.5
+            # ERROR CHECKER?
         else:
             print("Enter a value given.")
 
         # AI CHOOSES FROM RANDINT OF (INCLUDED,NOT INCLUDED)
         AI_CHOOSES = random.randint(1, number_inlist)
+
+
+
         PLAYER_CHOOSES = int(input(f"Your odds are a 1 in {number_inlist} chance. What Number Do You Choose?"))
         if PLAYER_CHOOSES == AI_CHOOSES:
             print("Yippee!")
             # ADD FUNCTION OF WIN + ADD MONEY HERE
+            Money.append(cash_gambled)
 
         else:
             print("Lose. :(")
             # ADD FUNCTION OF LOSE + SUBTRACT MONEY HERE
+            Money[0] -= cash_gambled
 
 
     def rockpaperscissors():
@@ -189,3 +194,6 @@ def main():
 
 
 main()
+
+
+
